e17cb017cf45d79b3493316b99eb649140d61b8c
PaulaLezan <paulalezn@gmail.com>
2022-07-18 01:06:12 -0300

    Final clase 20

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register (Curso)
+admin.site.register (Estudiante)
+admin.site.register (Profesor)
+admin.site.register (Entregable)
+
+
+
+
+
 
 # Register your models here.
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,3 +1,4 @@
+from ast import Return
 from django.db import models
 
 # Create your models here.
@@ -6,10 +7,18 @@ class Curso (models.Model):
     nombre = models.CharField(max_length=50)
     comision= models.IntegerField()
 
+    def __str__ (self):
+        return self.nombre+""+str(self.comision)
+
+
 class Estudiante (models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
+
+    def __str__ (self):
+        return self.nombre+""+self.apellido
+    
 
 class Profesor (models.Model):
     nombre = models.CharField(max_length=50)
@@ -17,7 +26,12 @@ class Profesor (models.Model):
     email = models.EmailField()
     profesion= models.CharField(max_length=50)
 
+    def __str__ (self):
+        return self.nombre
+
 class Entregable (models.Model):
     nombre = models.CharField(max_length=50)
     fecha_entrega= models.DateField()
     entregado= models.BooleanField()
+    def __str__ (self):
+        return self.nombre
